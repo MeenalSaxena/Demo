b355f351e296ad4006dac4517051dbf69a588ff7
MeenalSaxena <minilovely21@gmail.com>
2018-02-13 14:43:20 +0530

    Updated

--- ratingDirective/rating.js
+++ ratingDirective/rating.js
@@ -1,22 +1,15 @@
-angular.module("module").directive("rating", function () {
+angular.module("module").directive("stars",function(){
+
     return {
-        templateUrl: "ratingDirective/rating.html",
-       
-        
+    
+    template:"<div class=\"ratings\"><div class=\"empty-stars\"></div><div class=\"full-stars\" style=\"width:{{rate*20}}%\"></div></div>",
+    
+    scope:{
+    
+    rate:"@"
+    
     }
-});
-
-
-myapp.controller('ratingController', function($scope,$rootScope, show) {
-
     
-    show.getData($scope,3);
+    }
     
-    
-    //$scope.text = { "text-align":"center"};
-   
-   });
-   
-
-
-
+    })
